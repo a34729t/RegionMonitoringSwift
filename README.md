76fc9ec2826579e763980b0f9c505643ef0efec0
Nicolas Flacco <ncf1989@yahoo.com>
2014-06-25 19:54:17 -0700

    added BeaconManager

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-RegionMonitoringSwift
+Region Monitoring (in Swift)
 =====================
 
 iBeacon region monitoring, in Swift
+
+## Design
+
+The app centers around a singleton BeaconManager class, which tells the app about beacons that have been discovered. This is more or less copied from my triangulation app (https://https://github.com/a34729t/TriangulatorSwift) with the addition or region entry/exit delegate methods. And of course, background mode is enabled, and the background capabilties are enabled (detects BLE accessories, and location updates).
+
+## Testing
+
+This works on an iPhone 4S running iOS 7.1, with Estimote iBeacons.
+
+## CoreLocation vs CoreBluetooth
+
+I am using CoreLocation, which is the only accepted way of interating with iBeacons. CoreBluetooth doesn't give you the background capabilities needed in a distance-aware app. 
+
+CoreLocation gives you iBeacon class (proximity UUID), major and minor (think of them as ways to give an iBeacon a unique ID). CoreBluetooth gives you only a deviceUUID. In fact, the information available to CoreLocation and CoreBluetooth is  mutually exclusive- they handle the iBeacon advertisements differently.
---
+++ RegionMonitoringSwift/BeaconManager.swift
@@ -0,0 +1,94 @@
+//
+//  BeaconManager.swift
+//  RegionMonitoringSwift
+//
+//  Created by Nicolas Flacco on 6/25/14.
+//  Copyright (c) 2014 Nicolas Flacco. All rights reserved.
+//
+
+import CoreLocation
+import UIKit
+
+// config
+// General search criteria for beacons that are broadcasting
+let BEACON_SERVICE_NAME = "estimote"
+let BEACON_PROXIMITY_UUID = NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")
+
+// Beacons are hardcoded into our app so we can easily filter for them in a noisy environment
+let BEACON_PURPLE_MAJOR = "15071"
+let BEACON_PURPLE_MINOR = "10507"
+let BEACON_GREEN_MAJOR = "45565"
+let BEACON_GREEN_MINOR = "64072"
+
+protocol BeaconManagerDelegate {
+    func discoveredBeacon(#major: String, minor: String, proximity: CLProximity) // NOTE: #major forces first parameter to be named in function call
+}
+
+class BeaconManager: NSObject, CLLocationManagerDelegate    {
+    var locationManager: CLLocationManager = CLLocationManager()
+    let registeredBeaconMajor: String[] = [BEACON_GREEN_MAJOR, BEACON_PURPLE_MAJOR]
+    let estimoteRegion: CLBeaconRegion = CLBeaconRegion(proximityUUID:BEACON_PROXIMITY_UUID, identifier:"Estimote Region")
+    var delegate: BeaconManagerDelegate?
+    
+    class var sharedInstance:BeaconManager {
+    return sharedBeaconManager
+    }
+    
+    init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func start() {
+        println("BM start");
+        locationManager.startMonitoringForRegion(estimoteRegion)
+    }
+    
+    func stop() {
+        println("BM stop");
+        locationManager.stopMonitoringForRegion(estimoteRegion)
+    }
+    
+    //  CLLocationManagerDelegate methods
+    
+    func locationManager(manager: CLLocationManager!, didStartMonitoringForRegion region: CLRegion!) {
+        println("BM didStartMonitoringForRegion");
+        locationManager.requestStateForRegion(region); // should locationManager be manager?
+    }
+    
+    func locationManager(manager: CLLocationManager!, didDetermineState state: CLRegionState, forRegion region: CLRegion!) {
+        println("BM didDetermineState \(state)");
+        
+        switch state {
+        case .Inside:
+            println("BeaconManager:didDetermineState CLRegionState.Inside");
+            locationManager.startRangingBeaconsInRegion(estimoteRegion) // should locationManager be manager?
+        case .Outside:
+            println("BeaconManager:didDetermineState CLRegionState.Outside");
+        case .Unknown:
+            println("BeaconManager:didDetermineState CLRegionState.Unknown");
+        default:
+            println("BeaconManager:didDetermineState default");
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: CLBeacon[]!, inRegion region: CLBeaconRegion!) {
+        println("BM didRangeBeacons");
+        
+        for beacon: CLBeacon in beacons {
+            // TODO: better way to unwrap optionals?
+            if let major: String = beacon.major?.stringValue? {
+                if let minor: String = beacon.minor?.stringValue? {
+                    let contained: Bool = contains(registeredBeaconMajor, major)
+                    let active: Bool = (UIApplication.sharedApplication().applicationState == UIApplicationState.Active)
+                    if contained && active {
+                        delegate?.discoveredBeacon(major: major, minor: minor, proximity: beacon.proximity)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+let sharedBeaconManager = BeaconManager()
