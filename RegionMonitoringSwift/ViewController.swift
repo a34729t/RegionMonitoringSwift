85f070e813066531e671db7fca4efd22936ded76
Nicolas Flacco <ncf1989@yahoo.com>
2014-06-25 18:46:56 -0700

    initial commit

---
+++ RegionMonitoringSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RegionMonitoringSwift
+//
+//  Created by Nicolas Flacco on 6/25/14.
+//  Copyright (c) 2014 Nicolas Flacco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
