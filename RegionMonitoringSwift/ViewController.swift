6459c4b41c70b73a57df8be788e6f8ce39fb2fe4
Nicolas Flacco <nflacco@twitter.com>
2014-06-26 17:48:21 -0700

    updated ui stuff

--- RegionMonitoringSwift/ViewController.swift
+++ RegionMonitoringSwift/ViewController.swift
@@ -11,8 +11,22 @@ import UIKit
 import QuartzCore
 
 class ViewController: UIViewController {
+    // Singleton to manage location updates/ranging
     var beaconManager: BeaconManager?
-    
+
+    // UI elements
+    var beaconView: UIImageView?
+    var circleView: UIView?
+    var label: UILabel?
+
+    // UI colors and images
+    // let blueColor:UIColor = UIColor(red: 0.42, green: 0.75, blue: 0.87, alpha: 1.0) // No blue iBeacon
+    let greenColor:UIColor = UIColor(red: 0.49, green: 0.64, blue: 0.55, alpha: 1.0)
+    let purpleColor:UIColor = UIColor(red: 0.4, green: 0.34, blue: 0.65, alpha: 1.0)
+    let circleAlpha:CGFloat = 0.75
+    let beaconGreen = UIImage(named: "beaconGreen.png")
+    let beaconPurple = UIImage(named: "beaconPurple.png")
+
     override func viewDidLoad() {
         println("viewDidLoad")
         
@@ -25,21 +39,61 @@ class ViewController: UIViewController {
             // TODO: Alert, once alerts work without crashing app
         }
         
-        // Draw UI elements
-//        let circleView:CircleView = CircleView(frame: self.view.frame, width: 5.0)
-//        circleView.alpha = 0.25;
-//        self.view.addSubview(circleView);
-        
-        let circleView:UIView = UIView(frame: CGRectMake(10,20,100,100))
-        circleView.alpha = 0.5;
-        circleView.layer.cornerRadius = 50;
-        circleView.backgroundColor = UIColor.greenColor()
+
+        // Draw UI?
+        self.beaconView = drawBeacon(beaconPurple)
+        self.circleView = drawCircle(purpleColor)
+        self.label = drawLabel(purpleColor, message: "Inside Purple Region")
+
         self.view.addSubview(circleView);
+        self.view.addSubview(beaconView);
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // View Helpers
+
+    func drawLabel(color:UIColor, message:String) -> UILabel {
+        var label = UILabel(frame: CGRectMake(0, 0, 400, 100))
+        label.center = CGPointMake(self.view.bounds.size.width / 2, 50)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = message
+        label.textColor = color
+        label.font = UIFont.systemFontOfSize(30)
+        return label
+    }
+
+    func drawCircle(color:UIColor) -> UIView {
+
+        let diameter:CGFloat = min(self.view.bounds.size.width, self.view.bounds.size.height) / 1.2
+        let x = (self.view.bounds.size.width / 2) - (diameter / 2)
+        let y = (self.view.bounds.size.height / 1.7) - (diameter / 2)
+
+        let circleView:UIView = UIView(frame: CGRectMake(x, y, diameter, diameter))
+        circleView.layer.cornerRadius = diameter / 2;
+        circleView.backgroundColor = color
+        circleView.alpha = circleAlpha
+
+        return circleView;
+    }
+
+    func drawBeacon(image:UIImage) -> UIImageView {
+
+//        let scale:CGFloat = min(self.view.bounds.size.width, self.view.bounds.size.height)
+        let width = image.size.width
+        let height = image.size.height
+        let x = (self.view.bounds.size.width / 2) - (width / 2)
+        let y = (self.view.bounds.size.height / 1.7) - (height / 2)
+
+        let imageView: UIImageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: height))
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+
+        return imageView;
     }
     
 }
