d773089c525ddc3a37baba3a43d791234891abf5
Nicolas Flacco <nflacco@twitter.com>
2014-06-27 15:18:32 -0700

    zero badges on wake up

--- RegionMonitoringSwift/AppDelegate.swift
+++ RegionMonitoringSwift/AppDelegate.swift
@@ -33,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        application.applicationIconBadgeNumber = 0
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
